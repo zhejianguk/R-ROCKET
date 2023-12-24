33e99abbf384b41e0566d635f4794be8af3f29fc
zhejiang <zhejiang.uk@gmail.com>
2023-12-24 16:52:16 +0000

    Building a 4-core system.

--- guardiancouncil/GH_GlobalParams.scala
+++ guardiancouncil/GH_GlobalParams.scala
@@ -2,9 +2,9 @@ package freechips.rocketchip.guardiancouncil
 
 
 object GH_GlobalParams {
-  val GH_NUM_CORES = 5;
+  val GH_NUM_CORES = 4;
   val GH_WIDITH_PACKETS = 144;
-  val GH_DEBUG = 0;
+  val GH_DEBUG = 1
   val GH_WIDITH_PERF = 64;
 }
 
